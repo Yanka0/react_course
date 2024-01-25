d079b63ff3947961c29cc2082fba992a11c74ad0
Yana Savchenko <yana.savchenko1407@gmail.com>
2024-01-25 17:52:44 +0100

    Lecture 3 squashed commit

---
+++ src/utils/button/Button.tsx
@@ -0,0 +1,16 @@
+import {FunctionComponent, ReactNode} from 'react';
+
+
+type Props = {
+    children: ReactNode,
+    className?: string,
+    onClick: () => void,
+    isDisabled?: boolean
+};
+
+const Button: FunctionComponent<Props> = ({children, className, onClick, isDisabled}) => {
+
+    return (<button className={className} onClick={onClick} disabled={isDisabled}>{children}</button>);
+};
+
+export default Button;
